bb4942d3cedad6e28c67512f696621f61ae1f732
Zhe Yang <771874135@qq.com>
2018-11-24 21:18:59 +0800

    展示demo完成

---
+++ data/posts-data.js
@@ -0,0 +1,46 @@
+var local_database = [
+	{
+		date: "Sep 18 2016",
+		title: "从14年到18年; 从F20到F87",
+		post_img: "/images/m2.png",
+		author_img: "/images/avatar.png",
+        headimgsrc: "/images/avatar.png",
+        author: "橙子",
+        datetime: "24小时前",
+        detail: "踩住刹车油门到底，转速最高可以上升到3000rpm，不过这样出去的话，后轮打滑比较严重，时间损失反而更多。所以我们在尝试过后发现，在2500rpm左右，此时起步表现更理想，伴随着轮胎轻微的一声“抗议”，活蹦乱跳的动力就像小学生被老师领着去公园那样，有条不紊地涌过来，感觉还挺轻快的。只是这股轻快在经历了两次换挡之后，就变得越来越有气无力了。",
+		content: "M2上市的时候，媒体评测一致好评，大猩猩又出来放言说是近年来最好开的bmw，然后我就看看，价格上还是很坚挺，16年一度还要加价，车主们各种提车70、80+，真为那年车主感到心疼。",
+		view_num: "112",
+		collect_num: "101",
+		postid:0,
+        music : {
+            url: 'https://m10.music.126.net/20181118225716/089c8c82aca204796b7919832033bed1/ymusic/c8a6/4276/46d1/0b3badac3246d31d3a785cd499eccb7d.mp3',
+            title: '逍遥叹-胡歌',
+            coverImg: 'http://p2.music.126.net/4tTN8CnR7wG4E1cauIPCvQ==/109951163240682406.jpg?param=130y130'
+        }
+	},
+	{
+		date: "Sep 18 2018",
+		title: "从14年到18年; 从F20到F87",
+		post_img: "/images/m2.png",
+		author_img: "/images/avatar.png",
+        headimgsrc: "/images/avatar.png",
+        author: "橙子",
+        datetime: "24小时前",
+        detail: "踩住刹车油门到底，转速最高可以上升到3000rpm，不过这样出去的话，后轮打滑比较严重，时间损失反而更多。所以我们在尝试过后发现，在2500rpm左右，此时起步表现更理想，伴随着轮胎轻微的一声“抗议”，活蹦乱跳的动力就像小学生被老师领着去公园那样，有条不紊地涌过来，感觉还挺轻快的。只是这股轻快在经历了两次换挡之后，就变得越来越有气无力了。",
+		content: "M2上市的时候，媒体评测一致好评，大猩猩又出来放言说是近年来最好开的bmw，然后我就看看，价格上还是很坚挺，16年一度还要加价，车主们各种提车70、80+，真为那年车主感到心疼。",
+		view_num: "112",
+		collect_num: "96",
+		postid:1,
+        music: {
+            url: 'https://m10.music.126.net/20181120231209/38e69cda910e19b6b733161d73409250/ymusic/2242/0271/8dd1/b6da3ffc8d2561b321284004c81da136.mp3',
+            title: '年少有为-李荣浩',
+            coverImg: 'http://p1.music.126.net/tt8xwK-ASC2iqXNUXYKoDQ==/109951163606377163.jpg?param=130y130'
+        }
+	}
+
+]
+
+// 注册数据出口
+module.exports = {
+	postList: local_database 
+}
---
+++ pages/posts/post-detail/post-detail.js
@@ -0,0 +1,176 @@
+var postdatas = require('../../../data/posts-data.js')
+var app = getApp();
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        isPlayingMusic: false
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var postid = options.id;
+        // 传递数据到上面的data
+        this.data.currentPostid = postid
+        this.setData({
+            postdata: postdatas.postList[postid]
+        });
+
+        var postsCollected = wx.getStorageSync('posts_Collected')
+        if (postsCollected) {
+            var postCollected = postsCollected[postid];
+            if (postCollected) {
+                this.setData({
+                    collected: postCollected
+                })
+            } else {
+                postsCollected[postid] = false;
+                wx.setStorageSync('posts_Collected', postsCollected);
+            }
+        }
+        else {
+            postsCollected = {};
+            postsCollected[postid] = false;
+            wx.setStorageSync('posts_Collected', postsCollected);
+        }
+
+        if (app.globalData.g_isPlayingMusic && app.globalData.g_currentMusicPostId === postid){
+            this.setData({
+                isPlayingMusic: true
+            })
+        }
+
+        var that = this;
+        wx.onBackgroundAudioPlay(function () {
+            that.setData({
+                isPlayingMusic: true
+            })
+            app.globalData.g_isPlayingMusic = true;
+            app.globalData.g_currentMusicPostId = that.data.currentPostid;
+        });
+
+        wx.onBackgroundAudioPause(function () {
+            that.setData({
+                isPlayingMusic: false
+            })
+            app.globalData.g_isPlayingMusic = false;
+            app.globalData.g_currentMusicPostId = null;
+        })
+    },
+
+    onCollectionTap: function (event) {
+        var postsCollected = wx.getStorageSync('posts_Collected');
+        var postCollected = postsCollected[this.data.currentPostid];
+        // 收藏与不收藏相互转换
+        postCollected = !postCollected;
+        postsCollected[this.data.currentPostid] = postCollected;
+        // 更新文章是否收藏的缓存值
+        wx.setStorageSync('posts_Collected', postsCollected);
+        // 更新数据绑定变量。从而实现切换图片
+        this.setData({
+            collected: postCollected
+        })
+
+        wx.showToast({
+            title: postCollected ? '收藏成功' : '取消成功',
+            duration: 1000,
+        })
+    },
+
+    onShareTap: function (event) {
+        var itemList = [
+            "分享给微信好友",
+            "分享到朋友圈",
+            "分享到QQ",
+            "分享到微博"
+        ]
+        wx.showActionSheet({
+            itemList: itemList,
+            itemColor: "#405f80",
+            success: function (res) {
+                // res.cancel    用户是否点击了取消
+                // res.tapIndex 用户点击了选项的索引
+                wx.showModal({
+                    title: '确定' + itemList[res.tapIndex],
+                    content: '',
+                })
+            },
+        })
+    },
+
+    onMusicTap: function (event) {
+        var currentPostid = this.data.currentPostid
+        var postdata = postdatas.postList[currentPostid]
+        var isPlayingMusic = this.data.isPlayingMusic;
+        if (isPlayingMusic) {
+            wx.pauseBackgroundAudio();
+            this.setData({
+                isPlayingMusic: false
+            })
+        }
+        else {
+            wx.playBackgroundAudio({
+                dataUrl: postdata.music.url,
+                title: postdata.music.title,
+                coverImgUrl: postdata.music.coverImg
+            })
+            this.setData({
+                isPlayingMusic: true
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
